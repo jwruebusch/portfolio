3abb54ca28998cc252f4c3d9b5b84b5e494d5703
jwruebusch <jwruebusch@winsupplyinc.com>
2024-11-04 15:09:03 -0500

    environment file update

--- src/app/services/email-service/email.service.ts
+++ src/app/services/email-service/email.service.ts
@@ -2,24 +2,25 @@ import { Injectable } from '@angular/core';
 import emailJS from "emailjs-com";
 import {catchError, from, Observable, tap, throwError} from "rxjs";
 import {AlertService} from "../alert-service/alert.service";
+import {environment} from "../../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmailService {
-  private serviceId = process.env['EMAIL_SERVICE_ID'] ?? '';
-  private templateId = process.env['EMAIL_TEMPLATE_ID'] ?? '';
-  private publicId = process.env['EMAIL_PUBLIC_ID'] ?? '';
-
   constructor(private alertService: AlertService) { }
   
   sendEmail(contactInfo: any): Observable<any> {
-    return from(emailJS.send(this.serviceId, this.templateId, contactInfo, this.publicId)).pipe(
+    return from(this.emailPromise(contactInfo)).pipe(
       tap(() => this.alertService.addSuccess('Email sent successfully')),
       catchError((error) => {
         this.alertService.addError('Failed to send email');
         return throwError(() => error);
       })
     );
+  }
+  
+  private emailPromise(contactInfo: any): Promise<any> {
+    return emailJS.send(environment.EMAIL_SERVICE_ID, environment.EMAIL_TEMPLATE_ID, contactInfo, environment.EMAIL_PUBLIC_ID);
   }
 }
